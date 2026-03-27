785fef1f95a86bca61dc0db026f54f0dfae9786c
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2026-03-27 20:35:26 +0530

    Update vpc.tf

--- Devops/Terraform/vpc.tf
+++ Devops/Terraform/vpc.tf
@@ -1,48 +1,88 @@
-
-# create vpc
 resource "aws_vpc" "vnet" {
   cidr_block = "192.168.0.0/16"
   tags = {
-    Name = "vpc-tf"
+    Name = "tf-vpc"
   }
 }
 
-# create subnet
 resource "aws_subnet" "pub" {
   vpc_id                  = aws_vpc.vnet.id
   cidr_block              = "192.168.0.0/22"
   availability_zone       = "ap-southeast-1a"
   map_public_ip_on_launch = true
   tags = {
-    Name = "public-subnet"
+    Name = "tf-subnet-pub"
   }
 }
 
-#create internet gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vnet.id
   tags = {
-    Name = "igw-vpc-tf"
+    Name = "tf-igw"
   }
 }
 
-# create route table and attach igw
-resource "aws_route_table" "rt" {
+resource "aws_route_table" "rt-1" {
   vpc_id = aws_vpc.vnet.id
   tags = {
-    Name = "RT-Public"
+    Name = "rt-public"
   }
-
   route {
-    gateway_id = aws_internet_gateway.igw.id
     cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+}
+
+resource "aws_route_table_association" "rta-1" {
+  subnet_id      = aws_subnet.pub.id
+  route_table_id = aws_route_table.rt-1.id
+}
+
+
+
+# create security group
+resource "aws_security_group" "firewall" {
+  vpc_id = aws_vpc.vnet.id
+  name   = "tf-vpc-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
-# attach subnet to route table
-resource "aws_route_table_association" "rta" {
-  route_table_id = aws_route_table.rt.id
-  subnet_id      = aws_subnet.pub.id
+
+resource "aws_instance" "vm" {
+  ami                    = "ami-0be9cb9f67c8dabd6"
+  instance_type          = "t3.micro"
+  key_name               = "id_rsa"
+  user_data              = <<-EOF
+                #!/bin/bash
+                sudo -i
+                yum update -y
+                yum install httpd -y
+                systemctl start httpd
+                systemctl enable httpd
+                echo "Hello World from Terraform" > /var/www/html/index.html
+                EOF
+  subnet_id              = aws_subnet.pub.id
+  vpc_security_group_ids = [aws_security_group.firewall.id]
+  tags = {
+    Name = "webserver"
+  }
 }
-
-
-
